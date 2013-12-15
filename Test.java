92944b9592c9398f7e3a56a163335ca5bbefc922
DonRenando <renan.husson@hotmail.fr>
2013-12-15 22:00:49 +0100

    ok

---
+++ Test.java
@@ -0,0 +1,21 @@
+import jfilechooserdemo.*;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		
+		az mv;
+		mv = new az("CURLING");
+		mv.setVisible(true);
+
+	}
+
+}
+
+
+
+
