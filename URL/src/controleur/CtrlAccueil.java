de38e61a3c8171becf33fd1cadb305ccf7de5f79
DonRenando <renan.husson@hotmail.fr>
2013-12-19 14:20:52 +0100

    avec curseur wait sur bouton valider et message

---
+++ URL/src/controleur/CtrlAccueil.java
@@ -0,0 +1,30 @@
+package controleur;
+
+import vue.*;
+
+public class CtrlAccueil {
+	
+	
+	/**
+	 * Permet de creer une nouvelle boite de dialogue, DlgAccueil<BR>
+	 * @return		retourne la boite de dialgue DlgAccueil avec son Controleur en paramètre
+	 */
+	public DlgAccueil newDlgAccueil(){
+		return (new DlgAccueil(this)); 
+	}
+	
+	
+	
+	/**
+	 * Permet d'afficher la liste des url defectueux en creant un nouveau controlleur. 
+	 * Ce nouveau controlleur s'occupe lui même de creer la boite de dialogue DlgListe<BR>
+	 * @param		pNomFichier			Le nom de l'utilisateur
+	 */
+	public void afficherListe(String pNomFichier){
+		// On crée la nouvelle fenêtre par l'intermédiaire de CtrlListe;
+		CtrlListe newCtrl= new CtrlListe(pNomFichier);
+		newCtrl.newDlgListe();
+	}
+	
+
+}
---
+++ URL/src/principal.java
@@ -0,0 +1,10 @@
+import controleur.*;
+
+public class principal {
+
+	public static void main(String[] args) {
+		
+		CtrlAccueil monCtrl= new CtrlAccueil(); 
+		monCtrl.newDlgAccueil();
+	}
+}
