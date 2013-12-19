ec40ecb9677a2d11072972ee7ada065e6d1b4d24
matthieupenchenat81 <matthieu-p93@live.fr>
2013-12-19 19:34:30 +0100

    MAJ2

---
+++ Developpement/URL/src/Test/TestRegex.java
@@ -0,0 +1,39 @@
+package Test;
+import java.io.File;
+import java.io.IOException;
+
+import java.util.Scanner;
+
+	public class TestRegex {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		
+		
+		// Ce main permet de savoir si le regex qui sert à relever les liens est bien fonctionnel
+		// Les liens relevés sont affichés à l'écran.
+		
+		// Nous utilisons la classe Scanner pour utiliser de notre regex.
+		try {
+		Scanner scanner = new Scanner(new File("test.txt"));
+		String regex = "\\b(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]";
+		
+		while (scanner.hasNext()) {
+		
+		    String l =scanner.next();
+		          
+		    Scanner sc = new Scanner(l);
+		    String l2 =  sc.findInLine(regex);
+		       
+		    if (l2 != null)
+		    	System.out.println(l2);
+		}
+		
+					
+		} catch(IOException ignore){}
+	}
+
+}
